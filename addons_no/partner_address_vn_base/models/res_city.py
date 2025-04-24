e3fef1a69c0ced3a2745c9bfcd46813e3eff6ad9
Linh <19521760@gm.uit.edu.vn>
2025-04-24 15:57:28 +0700

    Add Addons

--- addons_no/partner_address_vn_base/models/res_city.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class City(models.Model):
-    _inherit = 'res.city'
-    _order = 'sequence'
-
-    sequence = fields.Integer(default=10)
-    code = fields.Char('Code')
